d875bbe353ace36c284a962bac9809353e9a2c23
kengo2003 <saitokengo2003@gmail.com>
2024-09-23 12:16:01 +0900

    feat: hightcharts導入

--- src/app/components/GraphDisplayArea.tsx
+++ src/app/components/GraphDisplayArea.tsx
@@ -1,5 +1,31 @@
-const GraphDisplayArea = () => {
-  return <div>グラフ表示</div>;
+"use client";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { useRef } from "react";
+
+const GraphDisplayArea = (props: HighchartsReact.Props) => {
+  const options: Highcharts.Options = {
+    title: {
+      text: "グラフ",
+    },
+    series: [
+      {
+        type: "line",
+        data: [1, 2, 3, 5, 3, 10, 0, 1, 1.2, 3.4],
+      },
+    ],
+  };
+
+  const chartComponentRef = useRef<HighchartsReact.RefObject>(null);
+
+  return (
+    <HighchartsReact
+      highcharts={Highcharts}
+      options={options}
+      ref={chartComponentRef}
+      {...props}
+    />
+  );
 };
 
 export default GraphDisplayArea;
